ebd7d5e6890d71661f489ed31ec547062c207625
Ramil Domingo CB <ramil.domingo@connectedbusiness.com>
2021-08-03 13:32:53 +0800

    Updated to v22.24

--- www/js/view/common/global.js
+++ www/js/view/common/global.js
@@ -10,7 +10,7 @@ define(
 	            UnitMeasureSystem: ''
 	        },
 	        CurrentTransactions: null,
-	         DemoServiceUrl: "http://data.connectedbusiness.com/demo/pos17/",
+	         DemoServiceUrl: "https://data.connectedbusiness.com/demo/pos22/",
 	        //DemoServiceUrl: "http://webservices.connectedbusiness.com/CBPOS_14_0_1/",
 	        //DemoServiceUrl: "http://webservices.connectedbusiness.com/CW/",
             IsBrowserMode : true,
@@ -31,8 +31,8 @@ define(
 	        Version14:  "19.0.0.0",
 	        Versions: {
 	            MinimumVersion: { Major: 19, Minor: 0, Build: 6914, Revision: 0 },
-	            Version: { Major: 19, Minor: 1, Build: 7031, Revision: 0},
-	            CurrentVersion: { Major: 19, Minor: 1, Build: 7031, Revision: 0
+	            Version: { Major: 22, Minor: 24, Build: 7884, Revision: 0},
+	            CurrentVersion: { Major: 22, Minor: 24, Build: 7884, Revision: 0
                 }
             },
             ScreenWidth: 0
