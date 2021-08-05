0dd92c540c1acb1258bb6602c58b59476ce3f01e
deti-sandeep <sandeep@dynenttech.com>
2021-08-05 15:50:12 +0530

    issue #68
    get rid of the cb.connectedbusiness.airprinter plugin

--- www/js/view/base/printer.js
+++ www/js/view/base/printer.js
@@ -1,4 +1,4 @@
-﻿define([    
+﻿define([
     'view/common/global',
     'view/common/shared',
     'view/common/enum',
@@ -6,13 +6,13 @@
     'view/common/method',
     'model/cartItem',
     'collection/cart',
-], function (Global, Shared, Enum, Service, Method,
+], function(Global, Shared, Enum, Service, Method,
     CartItemModel, CartCollection) {
     var Printer = {
-        DeleteLabelImages: function (labelNames) {
+        DeleteLabelImages: function(labelNames) {
             //if (labelNames != null && labelNames.length > 0) {
             //    var successPrintouts = new CartCollection();
-                
+
             //    labelNames.forEach(function (labelFileName) {
             //        var newItem = new CartItemModel();
             //        newItem.set({
@@ -31,20 +31,20 @@
             //}
         },
 
-        GetLabelFileName: function (pack) {            
+        GetLabelFileName: function(pack) {
             if (pack instanceof Backbone.Model) return pack.get("LabelFileName");
-            else return pack.LabelFileName;            
-        },       
+            else return pack.LabelFileName;
+        },
 
-        InitializeOptions: function (options) {
+        InitializeOptions: function(options) {
             if (options == null) options = { success: null, failed: null };
             return options;
         },
 
-        PrintReceiveLabelReport: function (purchaseReceipt, options) {
+        PrintReceiveLabelReport: function(purchaseReceipt, options) {
             options = this.InitializeOptions(options);
             options.printingMode = "receive";
-       
+
             if (purchaseReceipt != null) {
                 var fileName = purchaseReceipt.ReceiveLabelImageFile;
                 var imageUrl = Shared.GetImageUrl(Enum.ImageType.ReceiveLabel, fileName);
@@ -53,35 +53,34 @@
                 this.PrintReport(imageUrl, options);
             }
         },
-        
-        PrintLabel: function (pack, options) {
+
+        PrintLabel: function(pack, options) {
             options = this.InitializeOptions(options);
             options.printingMode = "label";
-       
+
             if (pack != null) {
                 var fileName = this.GetLabelFileName(pack);
                 var imageUrl = Shared.GetImageUrl(Enum.ImageType.Label, fileName);
-                
+
                 options.labels = [fileName];
                 this.PrintReport(imageUrl, options);
-            }
-            else {
+            } else {
                 var progress = options.progress;
                 if (progress != null) progress.HideProgress();
             }
         },
 
-        PrintLabels: function (packs, options) {            
+        PrintLabels: function(packs, options) {
             options = this.InitializeOptions(options);
             options.printingMode = "label";
-       
+
             if (packs != null && packs.length > 0) {
                 var labelNames = new Array();
                 var imageUrls = new Array();
                 var counter = 0;
                 var self = this;
 
-                packs.forEach(function (pack) {
+                packs.forEach(function(pack) {
                     if (pack != null) {
                         var fileName = self.GetLabelFileName(pack);
 
@@ -93,20 +92,19 @@
 
                 options.labels = labelNames;
                 this.PrintReport(imageUrls.toString(), options);
-            }
-            else {                
+            } else {
                 var progress = options.progress;
                 if (progress != null) progress.HideProgress();
             }
         },
 
-        PrintReport: function (imageURLString, options) {
+        PrintReport: function(imageURLString, options) {
             var printingMode = "default-printing";
-       
+
             var success = null;
             var error = null;
             var labels = null;
-       
+
             var self = this;
             var progress = options.progress;
 
@@ -116,71 +114,68 @@
                 labels = options.labels;
                 printingMode = options.printingMode;
             }
-       
+
 
             options = {
-                success: function (result) {
+                success: function(result) {
                     if (progress != null) progress.HideProgress();
                     if (success) {
                         self.DeleteLabelImages(labels);
                         success();
                     }
                 },
-                error: function (result) {
+                error: function(result) {
                     if (progress != null) progress.HideProgress();
 
                     var message = null;
 
                     if ((typeof result) == "string") {
                         message = result;
-                    }
-                    else {
+                    } else {
                         message = result.message;
                     }
 
                     navigator.notification.alert(message, null, "Print Error", "OK");
                     if (error) error();
                 },
-                printingMode : printingMode
+                printingMode: printingMode
             }
 
             this.ExecutePrintReport(imageURLString, options);
         },
 
-        ExecutePrintReport: function (imageURL, options) {
+        ExecutePrintReport: function(imageURL, options) {
             try {
                 //if (Global.IsBrowserMode) throw "Printing is not supported in desktop mode.";
-       
-                var printingMode = (typeof options.printingMode == "undefined" || options.printingMode=="") ? "default-printing" : options.printingMode;
+
+                var printingMode = (typeof options.printingMode == "undefined" || options.printingMode == "") ? "default-printing" : options.printingMode;
                 this.LoadAirPrintPlugin();
-                window.plugins.printPlugin.print( imageURL, options.success, options.error, printingMode );
-            }
-            catch (ex) {
+                window.plugins.printPlugin.print(imageURL, options.success, options.error, printingMode);
+            } catch (ex) {
                 //if (options.success) options.success(ex);
                 if (options.error) options.error(ex);
             }
         },
 
-        LoadAirPrintPlugin: function () {
+        LoadAirPrintPlugin: function() {
             if (!window.plugins) {
                 window.plugins = {};
             }
 
-            if (window.plugins.printPlugin == null) {                
+            if (window.plugins.printPlugin == null) {
                 if (cordova == null) throw "Unable to initialize printer plugin.";
-                
-                window.plugins.printPlugin = cordova.require(Global.Plugins.AirPrinter);
-                                
+
+                // window.plugins.printPlugin = cordova.require(Global.Plugins.AirPrinter);
+
 
                 if (window.plugins.printPlugin == null) throw "Unable to initialize printer plugin.";
-            }            
+            }
         },
 
-        RaiseError: function (error) {
+        RaiseError: function(error) {
             if (options.error) options.error(ex);
         }
     }
 
     return Printer;
-}
-);
+});
