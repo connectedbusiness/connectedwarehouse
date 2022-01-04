754e342b671f0197ed7ce7447d0d2cc5fc71e460
deti-sandeep <sandeep@dynenttech.com>
2022-01-04 18:44:43 +0530

    Fixed issues
    Fixed issues #24, #31, #33, #48, #49, #51

---
+++ typings/cordova-typings.d.ts
@@ -0,0 +1,4 @@
+
+/// <reference path="..\.vscode\typings\cordova\cordova.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\plugins\Dialogs.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\plugins\NetworkInformation.d.ts"/>
