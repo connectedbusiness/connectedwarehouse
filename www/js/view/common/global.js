40840482fa7dbfdfa78ba02b46c42500f26a8495
Ramil Domingo <ramil.domingo@connectedbusiness.com>
2023-02-09 20:53:59 +0800

    Update to 23.0.8427.7.

--- www/js/view/common/global.js
+++ www/js/view/common/global.js
@@ -27,9 +27,9 @@ define(
 	        WorkstationID: "Warehouse1",
 	        Version14:  "19.0.0.0",
 	        Versions: {
-	            MinimumVersion: { Major: 23, Minor: 0, Build: 8427, Revision: 6 },
-	            Version: { Major: 23, Minor: 0, Build: 8427, Revision: 6},
-	            CurrentVersion: { Major: 23, Minor: 0, Build: 8427, Revision: 6
+	            MinimumVersion: { Major: 23, Minor: 0, Build: 8427, Revision: 7 },
+	            Version: { Major: 23, Minor: 0, Build: 8427, Revision: 7 },
+	            CurrentVersion: { Major: 23, Minor: 0, Build: 8427, Revision: 7
                 }
             },
             ScreenWidth: 0
