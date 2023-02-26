1afef1cbf3156f0104b24f0d638c6819a6a60070
Ramil Domingo <ramil.domingo@connectedbusiness.com>
2023-02-26 13:05:48 +0800

    Bug fixing.

--- www/js/view/common/global.js
+++ www/js/view/common/global.js
@@ -27,9 +27,9 @@ define(
 	        WorkstationID: "Warehouse1",
 	        Version14:  "19.0.0.0",
 	        Versions: {
-	            MinimumVersion: { Major: 23, Minor: 0, Build: 8427, Revision: 14 },
-	            Version: { Major: 23, Minor: 0, Build: 8427, Revision: 14 },
-	            CurrentVersion: { Major: 23, Minor: 0, Build: 8427, Revision: 14
+	            MinimumVersion: { Major: 23, Minor: 0, Build: 8427, Revision: 15 },
+	            Version: { Major: 23, Minor: 0, Build: 8427, Revision: 15 },
+	            CurrentVersion: { Major: 23, Minor: 0, Build: 8427, Revision: 15
                 }
             },
             ScreenWidth: 0
