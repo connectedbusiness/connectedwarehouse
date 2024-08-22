9dc180e38bf82e7c6489937833184833489066f4
Ramil Domingo <ramil.domingo@innov8software.com>
2024-08-22 19:19:11 +0800

    Bug fixes.

--- www/js/view/common/global.js
+++ www/js/view/common/global.js
@@ -28,8 +28,8 @@ define(
 	        Version14:  "19.0.0.0",
 	        Versions: {
 	            MinimumVersion: { Major: 20, Minor: 0, Build: 7458, Revision: 1 },
-	            Version: { Major: 24, Minor: 0, Build: 8924, Revision: 1 },
-	            CurrentVersion: { Major: 24, Minor: 0, Build: 8924, Revision: 1
+	            Version: { Major: 24, Minor: 0, Build: 8998, Revision: 1 },
+	            CurrentVersion: { Major: 24, Minor: 0, Build: 8998, Revision: 1
                 }
             },
             ScreenWidth: 0
